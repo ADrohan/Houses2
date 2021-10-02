f8965c63ef2139da77f558d0dff70ded32307ec8
aileendrohan <aileendrohan@gmail.com>
2021-10-02 11:08:44 +0100

    button added with view binding

--- app/src/main/java/org/wit/houses/HouseActivity.kt
+++ app/src/main/java/org/wit/houses/HouseActivity.kt
@@ -2,16 +2,28 @@ package org.wit.houses
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.wit.houses.databinding.ActivityHouseBinding
 
 import timber.log.Timber
 import timber.log.Timber.i
 
 class HouseActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHouseBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_house)
+        binding = ActivityHouseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Timber.plant(Timber.DebugTree())
+
         i("House Activity started...")
+
+        binding.btnAdd.setOnClickListener() {
+            i("add Button Pressed")
+        }
+
+
     }
 }
