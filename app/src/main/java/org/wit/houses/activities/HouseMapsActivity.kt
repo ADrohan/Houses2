b5c194f3f9e8b3309c9c1e7e142247aa7df1bc10
aileendrohan <aileendrohan@gmail.com>
2021-12-10 21:36:41 +0000

    google maps showing and card placeholder in content

--- app/src/main/java/org/wit/houses/activities/HouseMapsActivity.kt
+++ app/src/main/java/org/wit/houses/activities/HouseMapsActivity.kt
@@ -2,18 +2,72 @@ package org.wit.houses.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import org.wit.houses.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import org.wit.houses.databinding.ActivityHouseMapsBinding
+import org.wit.houses.databinding.ContentHouseMapsBinding
+import org.wit.houses.main.MainApp
 
 class HouseMapsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHouseMapsBinding
+    private lateinit var contentBinding: ContentHouseMapsBinding
+    lateinit var map: GoogleMap
+    lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        app = application as MainApp
         binding = ActivityHouseMapsBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_house_maps)
+        //setContentView(R.layout.activity_house_maps)
+        setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+
+        contentBinding = ContentHouseMapsBinding.bind(binding.root)
+        contentBinding.mapView.onCreate(savedInstanceState)
+        contentBinding.mapView.getMapAsync {
+            map = it
+            configureMap()
+
+        }
+    }
+
+    fun configureMap() {
+        map.uiSettings.setZoomControlsEnabled(true)
+        app.houses.findAll().forEach {
+            val loc = LatLng(it.lat, it.lng)
+            val options = MarkerOptions().title(it.address).position(loc)
+            map.addMarker(options).tag = it.id
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        contentBinding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        contentBinding.mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        contentBinding.mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        contentBinding.mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        contentBinding.mapView.onSaveInstanceState(outState)
     }
 }
+
